d6964dcfdec8b377cda4778f980d2f3c00e25a49
hajnanavas <hajnanavas123@gmail.com>
2019-01-09 23:18:36 +0530

    modules added

--- src/app/pages/structure/structure.module.ts
+++ src/app/pages/structure/structure.module.ts
@@ -4,15 +4,18 @@ import { CommonModule } from '@angular/common';
 import { StructureRoutingModule } from './structure-routing.module';
 import { StructureCreateComponent } from './structure-create/structure-create.component';
 import { StructureListComponent } from './structure-list/structure-list.component';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MapsComponent } from 'src/app/shared/components/maps/maps.component';
+import { MatDialogModule, MatFormFieldModule, MatIconModule, MatSlideToggleModule, MatInputModule, MatSelectModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [StructureCreateComponent, StructureListComponent, MapsComponent],
   imports: [
     CommonModule,
     StructureRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule, MatSlideToggleModule, MatInputModule, MatSelectModule
   ],
   entryComponents: [StructureCreateComponent]
 })

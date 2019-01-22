f705d7915d01d4d5679fde016eaf87c12338122a
hajnanavas <hajnanavas123@gmail.com>
2019-01-22 15:38:00 +0530

    code structured

--- src/app/shared/components/info-window/info-window.component.ts
+++ src/app/shared/components/info-window/info-window.component.ts
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class InfoWindowComponent implements OnInit {
   param: any;
   strokeColor: string;
+
   constructor() {
   }
+  
   ngOnInit() {
     this.strokeColor = (this.param.occupiedSpace > this.param.low ? (this.param.occupiedSpace > this.param.medium ? (this.param.occupiedSpace > this.param.full ?  '#ff0000' : '#009900') : '#009900') : '#e7ea13');
   }
